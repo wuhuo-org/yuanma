7f25708dd6ee8e2c39c5d3bd12883b9864f7ec3c
yanghubin <857632811@qq.com>
2017-08-30 15:32:37 +0800

    修改主页

--- daima/index.php
+++ daima/index.php
@@ -5,7 +5,10 @@
 		<li><a href=wenzhang.php?name=null&type=boot>boot</a></li>
 		<li><a href=wenzhang.php?name=null&type=debug>debug </a></li>
 		<li><a href=wenzhang.php?name=null&type=driver>driver </a></li>
+		<li><a href=wenzhang.php?name=null&type=devs> devs </a></li>
 		<li><a href=wenzhang.php?name=null&type=fs>fs </a></li>
+		<li><a href=wenzhang.php?name=null&type=efs> efs </a></li>
+		<li><a href=wenzhang.php?name=null&type=vfs>vfs </a></li>
 		<li><a href=wenzhang.php?name=null&type=init>init </a></li>
 		<li><a href=wenzhang.php?name=null&type=libs>libs </a></li>
 		<li><a href=wenzhang.php?name=null&type=mm>mm </a></li>
@@ -14,6 +17,7 @@
 		<li><a href=wenzhang.php?name=null&type=sync>sync </a></li>
 		<li><a href=wenzhang.php?name=null&type=syscall>syscall </a></li>
 		<li><a href=wenzhang.php?name=null&type=trap>trap </a></li>
+		<li><a href=wenzhang.php?name=null&type=swap> swap </a></li>
 	</ul>
 <?php
 	require_once('end.php');
